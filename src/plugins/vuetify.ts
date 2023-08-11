cc92473495bf118b4f7408117b1b4fbba21e6652
Pedro Pardal <ppardalj@gmail.com>
2023-08-11 18:08:42 +0200

    feat: riftlink theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -11,16 +11,28 @@ import 'vuetify/styles'
 // Composables
 import { createVuetify } from 'vuetify'
 
+const riftlinkTheme = {
+  "dark": true,
+  "colors": {
+    "background": "#121212",
+    "surface": "#212121",
+    "primary": "#FF0B45",
+    "secondary": "#25E1BB",
+    "error": "#FF002E",
+    "info": "#25E1BB",
+    "success": "#3AAF14",
+    "warning": "#FB8C00",
+    "on-surface": "#EEEEEE",
+    "on-background": "#EEEEEE",
+  }
+};
+
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
+    defaultTheme: 'riftlinkTheme',
     themes: {
-      light: {
-        colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6',
-        },
-      },
+      riftlinkTheme
     },
   },
 })
