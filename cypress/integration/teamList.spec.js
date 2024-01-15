7cefd9a78fbe7fe9c7b3a043895155e4a841ad5f
Pedro Pardal <ppardalj@gmail.com>
2024-01-15 08:37:42 +0100

    test: Team list navigates to team detail

--- cypress/integration/teamList.spec.js
+++ cypress/integration/teamList.spec.js
@@ -9,4 +9,12 @@ describe('Team list', () => {
     cy.get('[data-cy=team-list]').should('have.length', 1)
     cy.get('[data-cy=team-list-item]').should('have.length', 7)
   })
+
+  it('navigates to team detail', () => {
+    const t1Card = cy.get('[data-cy=team-list-item]').first()
+
+    t1Card.find('[data-cy=see-more-button]').click()
+
+    cy.url().should('include', '/team/f2163c37-8931-4c4d-8a22-bf9a0bbaf6aa')
+  })
 })
