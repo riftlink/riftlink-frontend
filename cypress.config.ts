6426c7586822c7bd954c7f812feee8e9813b35ac
Pedro Pardal <ppardalj@gmail.com>
2024-01-16 08:01:16 +0100

    chore: install cucumber

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'cypress'
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    specPattern: "**/*.feature",
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
       return require('./cypress/plugins/index.js')(on, config)
     },
   },
